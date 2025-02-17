4f91c699babb5c56c79c2a4dc16e3888137b7f13
Mohsen Banan <form@mohsen.banan.1.byname.net>
2025-02-16 18:39:52 -0800

    0.31 un-unisos.

--- py3/bin/icmEx-clsMethod.py
+++ py3/bin/icmEx-clsMethod.py
@@ -1,5 +1,10 @@
 #!/bin/env python
 # -*- coding: utf-8 -*-
+
+import sys
+print(f"NOTYET, To be sorted out")
+sys.exit()
+
 """\
 * *[Summary]* :: A /library/ Beginning point for development of new ICM oriented libraries.
 """
@@ -89,20 +94,7 @@ icmInfo['cmndParts'] = "IcmCmndParts[common] IcmCmndParts[param]"
 """
 ####+END:
 
-####+BEGIN: bx:dblock:global:file-insert-cond :cond "./blee.el" :file "/bisos/apps/defaults/update/sw/icm/py/importUcfIcmBleepG.py"
-from unisos import ucf
-from unisos import icm
-
-icm.unusedSuppressForEval(ucf.__file__)  # in case icm and ucf are not used
-
-G = icm.IcmGlobalContext()
-# G.icmLibsAppend = __file__
-# G.icmCmndsLibsAppend = __file__
-
-from blee.icmPlayer import bleep
-####+END:
-
-from bisos.icm import clsMethod
+# from bisos.icm import clsMethod
 
 g_importedCmndsModules = [       # Enumerate modules from which CMNDs become invokable
     'blee.icmPlayer.bleep',
